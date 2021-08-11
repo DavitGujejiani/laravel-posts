93de41f35191abf2bc4cda54d98ffb2b312a41b4
DavitGujejiani <gujejiani.daviti@gmail.com>
2021-08-11 16:24:03 +0400

    post caching error fixed

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -2,88 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Error;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
-use Symfony\Component\VarDumper\VarDumper;
 
 class PagesController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return object
      */
-    public function index()
+    public function index(): object
     {
         $posts = cache()->remember('posts', 300, function () {
-            $response = Http::get('https://jsonplaceholder.typicode.com/posts');
-
-            if (!$response->ok()) {
-                return response()->json([
-                    'message' => 'Bad request',
-                ], 400);
-            }
-
-            return json_decode($response->body());
+            return Http::get('https://jsonplaceholder.typicode.com/posts')->object();
         });
 
         return view('/posts', compact('posts'));
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return object
      */
-    public function show($id)
+    public function show(int $id): object
     {
-        $post = cache()->remember("posts{$id}", 300, function () use ($id) {
-            $response = Http::get("https://jsonplaceholder.typicode.com/posts/{$id}");
+        $post = Cache::remember('posts' . $id, 300, function () use ($id) {
+            $response = Http::get('https://jsonplaceholder.typicode.com/posts/' . $id);
 
-            if (!$response->ok()) {
-                return response()->json([
-                    'message' => 'Post not found',
-                ], 404);
+            if ($response->failed()) {
+                abort(404);
             }
 
-            return json_decode($response->body());
+            return $response->object();
         });
 
         return view('/post', compact('post'));
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
